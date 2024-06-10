91e86acb8369111ee635df6e1b5461ae6b9a3667
Gunjan Mishra <113249136+Gunjanmishra481@users.noreply.github.com>
2024-06-11 02:40:36 +0530

    Update index.js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,5 +1,7 @@
 for(var i=0;i<document.querySelectorAll(".drum").length;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-        alert("I got clicked!");
+        var audio=new Audio(src="/Drum Kit Starting Files/sounds/tom-1.mp3");
+        audio.play();
     });
 }
+
